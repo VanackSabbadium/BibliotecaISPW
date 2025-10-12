b27013818fd97bd3aba01486deb56b4871773f79
VanackSabbadium <vanacksabbadium@vsservices.simplelogin.com>
2025-10-12 10:03:12 +0200

    Resolved some code smells

--- src/main/java/it/biblioteca/db/DatabaseConfig.java
+++ src/main/java/it/biblioteca/db/DatabaseConfig.java
@@ -42,7 +42,7 @@ public final class DatabaseConfig {
         try {
             DriverManager.getConnection(buildJdbcUrl(), user, pass).close();
             return true;
-        } catch (SQLException e) {
+        } catch (SQLException _) {
             return false;
         }
     }
